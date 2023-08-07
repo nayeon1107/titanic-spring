0d37c65aea9a62ecb01ec8423f8a3fc8d6b453cd
nayeon1107 <quf991107@gmail.com>
2023-08-07 10:29:49 +0900

    initial commit

---
+++ src/main/java/com/dmario/titanicuserapi/TitanicUserApiApplication.java
@@ -0,0 +1,13 @@
+package com.dmario.titanicuserapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitanicUserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TitanicUserApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dmario/titanicuserapi/TitanicUserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmario.titanicuserapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitanicUserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
